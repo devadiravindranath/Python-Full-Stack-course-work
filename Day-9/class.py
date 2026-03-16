567ede30ab961977ffa6e3d9883b44b2af0eb20f
DEVADI RAVINDRANATH <ravindranathdevadi@gmail.com>
2026-03-16 15:42:42 +0530

    added

---
+++ Day-9/class.py
@@ -0,0 +1,10 @@
+"""class abcd:
+    var=100
+object=abcd()
+print(object.var)"""
+
+
+class icici:
+    def __init__(self,cash) :
+        print("cash deposited: ",cash)
+cash=icici(1000)
---
+++ Day-9/quiz.py
@@ -0,0 +1,34 @@
+def run_quiz():
+
+questions=[
+    {"Question 1 ": 'Which keyword is used to define a function in Python?',
+     "options" : ["A.function","B.define","C.def","D.fun"]
+     "answer" :"A"}
+    {"Question 2 ": 'Which function is used to take input from the user in Python?',
+     "options" : ["A.function","B.input()","C.def","D.fun"]
+     "answer" :"B"}
+    {"Question 3 ": 'What is the correct way to create a list in Python?',
+     "options" : ["A.function","B.define","C.list = [1, 2, 3]","D.fun"]
+     "answer" :"C"}
+    
+    ]
+for i in data:
+    print(f'Question: {i[0]')
+    print(f'a. {i[1]}')
+    print(f'b. {i[2]}')
+    print(f'c. {i[3]}')
+    print(f'd. {i[4]}')
+    ans = input("your answer (a/b/c/d)")
+score = 0
+
+print("\n====python quiz=====\n")
+
+
+
+    
+
+
+
+
+
+    
