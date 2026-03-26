dc456475cdf497c255f446a7f6659e8746a49978
DEVADI RAVINDRANATH <ravindranathdevadi@gmail.com>
2026-03-26 15:51:21 +0530

    added

---
+++ Day-17/practice.py
@@ -0,0 +1,47 @@
+"""list = ["vicky","nani","bag","laptop"]
+list[2]="uday"
+print(list)"""
+
+#addin and removing list values.
+"""list = ["vicky","nani","bag","laptop"]
+list.remove("bag")
+list.append("uday")
+print(list)"""
+
+
+
+"""list = ["vicky","nani","bag","laptop"]
+lists = ["vicky"]
+for list in lists:
+    print(list)"""
+
+
+"""numbers=[1,2,3,4,5]
+for num in numbers :
+    print(num)"""
+
+"""list = ["vicky","nani","bag","laptop"]
+
+print(list[2])
+
+
+numbers=[1,2,3,4,5]
+numbers.extend([6,7,8])
+print(numbers)"""
+
+numbers=[1,2,3,4,5]
+numbers.pop()
+print(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
